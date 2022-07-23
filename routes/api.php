fe9e7e0d9e7c52bf82eba76c64391c969acce9b0
fadhluibnu <f.ibnu.a1@gmail.com>
2022-07-23 13:14:23 +0700

    API Login, Logout,

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\FormController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/form', [FormController::class, 'index']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
